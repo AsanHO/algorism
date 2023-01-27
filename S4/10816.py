5c296895f74dc4d1b79d6bcd4a17ead7be30cb77
AsanHO <gorma00@naver.com>
2023-01-27 11:33:28 +0900

    0127

--- S4/10816.py
+++ S4/10816.py
@@ -1,4 +1,5 @@
 import sys
+from collections import Counter
 os = sys.platform
 if os != "linux" :
     print("명재야 보아하니 백준에서 푼게 아니구나")
@@ -11,38 +12,10 @@ a = list(map(int,input().split()))
 M = int(input())
 m = list(map(int,input().split()))
 
-#이진탐색을 한후, 해당 값을 키값으로 사용ㅐ 해싱
-a.sort()
-result = []
-def bs (m,a):
-    for target in m:
-        start = 0
-        end = len(a) - 1
-        while start <= end:
-            mid = (start + end) //2    
-            if a[mid] == target:        
-                break
-            elif a[mid] < target:
-                start = mid + 1 
-            else:
-                end = mid - 1
-        
-
-
-bs(m,a)
-
-""" # 실버 4
-# 시간초가 나기 때문에 중복 수 제거해야함
-import sys
-input = sys.stdin.readline
-
-N = int(input())
-arr = set(map(int, input().split()))
-
-M = int(input())
-m_arr = list(map(int, input().split()))
-for m in m_arr:
-    if m in arr:
-        print(1)
-    else:
-        print(0) """
+c= Counter(a)
+print(c)
+for i in m:
+  if i in c:
+    print(c[i], end=' ')
+  else:
+    print(0, end=' ')
