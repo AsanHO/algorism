1dfb655f6da8ae4cb6253ed2ea3139373a291e09
AsanHO <gorma00@naver.com>
2023-02-17 23:58:14 +0900

    0217

---
+++ V5/1271.py
@@ -0,0 +1,11 @@
+import sys
+os = sys.platform
+if os != "linux" :
+    print("명재야 보아하니 백준에서 푼게 아니구나")
+    input=open("example.txt","rt").readline
+else: 
+    input=sys.stdin.readline
+
+n,m = map(int,input().split())
+print(n//m)
+print(n%m)
