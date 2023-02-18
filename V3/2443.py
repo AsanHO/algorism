fdba603f57c38ae286fc773502656d116ec87c72
AsanHO <gorma00@naver.com>
2023-02-18 23:15:51 +0900

    0218

---
+++ V3/2443.py
@@ -0,0 +1,27 @@
+import sys
+
+os = sys.platform
+if os != "linux" :
+    print("명재야 보아하니 백준에서 푼게 아니구나")
+    input=open("example.txt","rt").readline
+else: 
+    input=sys.stdin.readline
+
+target = int(input())
+count_list = []
+for i in range(target):
+    i += 1
+    count = 2*i-1
+    count_list.append(count)
+re_count_list = list(reversed(count_list))
+last = count_list[-1]
+for i in re_count_list:
+    blank_count = int((last - i) / 2)
+    blank = " "*(blank_count-1)
+    star = "*"*i
+    if i == last:
+        print(star)
+    elif i == 0:
+        break
+    else:
+        print(blank,star)
