82dc2c84ca8cb723bb4845e43155ab9636abf347
AsanHO <gorma00@naver.com>
2023-04-16 12:32:27 +0900

    0416

--- test.py
+++ test.py
@@ -1,5 +1,39 @@
-arr = [1, 10, 2, 3, 4, 5, 20, 11, 12, 32]
-arr.sort()
-print(arr)
-for idx, arr in enumerate(arr):
-    print(idx, arr)
+import sys
+os = sys.platform
+if os != "linux":
+    print("명재야 보아하니 백준에서 푼게 아니구나")
+    input = open("example.txt", "rt").readline
+else:
+    input = sys.stdin.readline
+
+
+def dfs(x, y):
+    for i in graph:
+        print(i)
+    print("-----------")
+    if x <= -1 or x >= n or y <= -1 or y >= m:
+        return False
+    if graph[x][y] == 0:
+        graph[x][y] = 1
+        dfs(x-1, y)
+        dfs(x, y-1)
+        dfs(x+1, y)
+        dfs(x, y+1)
+        return True
+    return False
+
+
+n, m = map(int, input().split())
+
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input().split())))
+
+result = 0
+for i in range(n):
+    for j in range(m):
+        print(i, j, dfs(i, j))
+        if dfs(i, j) == True:
+            result += 1
+
+print(result)
