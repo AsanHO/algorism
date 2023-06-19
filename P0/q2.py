2dbf841362c905d5ee09e549baf4f68a3fc92bd3
AsanHO <gorma00@naver.com>
2023-06-19 11:01:03 +0900

    0619

--- P0/q2.py
+++ P0/q2.py
@@ -76,6 +76,8 @@ def solution(arr):
 print(solution(["INT", "INT", "BOOL", "SHORT", "LONG"]))
 print(solution(["INT", "SHORT", "FLOAT", "INT", "BOOL"]))
 print(solution(["FLOAT", "SHORT", "BOOL", "BOOL", "BOOL", "INT"]))
+print(solution(["BOOL",
+      "LONG", "BOOL", "LONG", "SHORT", "LONG"]))
 print(solution(["BOOL", "LONG", "SHORT", "LONG", "BOOL",
       "LONG", "BOOL", "LONG", "SHORT", "LONG", "LONG"]))
 """
